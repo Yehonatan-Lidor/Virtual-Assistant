a4288442adadb385264a726bedb7bba0367749de
Yehonatan-Lidor <zizi150503@gmail.com>
2022-01-09 21:48:47 +0200

    Added script to create dataset of messaging requests

---
+++ Music&Weather/extract_msg.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+def main():
+    df = pd.read_csv('messaging_test.csv')
+    df = df.drop(columns=['domain'])
+    parsed = []
+    a  = ''
+    for parsed_sentence in df.itertuples():
+        temp = [parsed_sentence[1], parsed_sentence[2], '']
+        if 'IN:' in temp[1]:
+            temp[1]= temp[1].split('IN:')[1].split(' ')[0]
+            txt = parsed_sentence[2].split('IN:')[1]
+            index = txt.find('[')
+            temp[2] = txt[index : -1]
+            temp[0], temp[1] = temp[1], temp[0]
+
+            parsed.append(temp)
+    df = pd.DataFrame(list(parsed),
+    columns =['Intent', 'Query', "Param"])
+    df.to_csv('messaging.csv')
+
+
+if __name__ == '__main__':
+    main()
